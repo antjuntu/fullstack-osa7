a62c83e36f6466773495bc6c5420c75d3ed84fdf
antjuntu <antjuntu@gmail.com>
2019-08-13 21:20:25 +0300

    7.7 käyttäjien näkymä

--- bloglist-frontend/src/components/Users.js
+++ bloglist-frontend/src/components/Users.js
@@ -1,17 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+import { initializeUsers } from '../reducers/userReducer'
 
-const Users = () => {
+const Users = (props) => {
+  useEffect(() => {
+    props.initializeUsers()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
     <div>
       <h2>Users</h2>
       <table>
         <tbody>
           <tr><th></th><th>blogs created</th></tr>
-          {}
+          {props.users.map(user => <tr key={user.id}><td>{user.name}</td><td>{user.blogs.length}</td></tr>)}
         </tbody>
       </table>
     </div>
   )
 }
 
-export default Users
+const mapStateToProps = (state) => {
+  return {
+    users: state.users
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  {
+    initializeUsers
+  }
+)(Users)
