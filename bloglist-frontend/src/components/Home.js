c1c7b78ee92623a9b6bbf764fe40e75c530f5281
antjuntu <antjuntu@gmail.com>
2019-08-19 14:39:15 +0300

    Home.js refactored

--- bloglist-frontend/src/components/Home.js
+++ bloglist-frontend/src/components/Home.js
@@ -1,24 +1,33 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 import Togglable from './Togglable'
-import Blog from './Blog'
 import NewBlog from './NewBlog'
 
 const Home = (props) => {
 
   const byLikes = (b1, b2) => b2.likes - b1.likes
 
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
   return (
     <div>
       <Togglable buttonLabel='create'>
         <NewBlog />
       </Togglable>
-
+      <br />
       {props.blogs.sort(byLikes).map(blog =>
-        <Blog
-          key={blog.id}
-          blog={blog}
-        />
+        <div key={blog.id} style={blogStyle}>
+          <Link to={`/blogs/${blog.id}`}>
+            {blog.title} <i>by</i> {blog.author}
+          </Link>
+        </div>
       )}
     </div>
   )
